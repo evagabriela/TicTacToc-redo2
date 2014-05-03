fbd10cdb4bc83a0f6ab5bd9dea3925dbc134a5cd
evagabriela <gabriela.osu@gmail.com>
2014-05-03 14:00:09 -0500

    add test for redraw the board with an X in the location based on user input

--- test/GameTest.java
+++ test/GameTest.java
@@ -1,15 +1,18 @@
 import org.junit.Test;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintStream;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 
 public class GameTest {
     private PrintStream out;
     private Game game;
+    private BufferedReader reader;
 
     @Test
     public void shouldDrawEmptyGridWhenGameStarts() throws IOException {
@@ -24,13 +27,27 @@ public class GameTest {
     }
 
 //    Make a move
-// Redraw the board with an ‘X’ in that location. It doesn’t matter what happens if they enter anything besides a number from 1 to 9.
+// Redraw the board with an ‘X’ in that location.
     @Test
     public void shouldPromptUserToEnterAPosition(){
         out = mock(PrintStream.class);
         Game game = new Game(out);
         game.start();
         verify(out).println("Please enter a number between 1 and 9");
+    }
+
+    @Test
+    public void shouldRedrawBoardWithUserPositionInput() throws IOException {
+        out = mock(PrintStream.class);
+        reader = mock(BufferedReader.class);
+        when(reader.readLine()).thenReturn("1");
+        Game game = new Game(out);
+        game.start();
+        verify(out).println(" X| | \n"
+                + "-----\n"
+                + " | | \n"
+                + "-----\n"
+                + " | | \n");
     }
 
 }
